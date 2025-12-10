7d63878eac177fde8ffe0eae401536aff4825f18
Muhammad Sulton <mhammadslton46@gmail.com>
2025-12-10 23:18:34 +0700

    Add some tourism place

--- lib/Model/tourism_place.dart
+++ lib/Model/tourism_place.dart
@@ -23,21 +23,55 @@ class TourismPlace {
 }
 
 var tourismPlaceList = [
-TourismPlace(
-name: 'Gedung Miring Unsika',
-location: 'Telukjambe, Karawang',
-description:
-'Berada di Kampus Terbaik Di Alam Semesta yaitu Universitas Singaperbangsa Karawang. Selalu menjadi bahan perbincangan mahasiswa Fasilkom. Ketika anda melewati kawasan ini, anda akan mencium aura korupsi yang sangat kuat. Hanya Allah yang tau akhir dari nasib gedung ini.',
-openDays: 'Setiap Hari',
-openTime: '24 Jam Nonstop',
-ticketPrice: 'Gratis',
-imageAsset: 'images/gedung_miring_unsika.jpg',
-imageUrls: [
-'https://karawangpos.com/wp-content/uploads/2021/04/fslkm.jpg',
-'https://i0.wp.com/www.faktajabar.co.id/wp-content/uploads/2019/09/Gedung-Fasilkom_1.jpeg?fit=1036%2C492&ssl=1&resize=350%2C200',
-'https://i0.wp.com/www.faktajabar.co.id/wp-content/uploads/2022/07/IMG-20220713-WA0040.jpg?fit=1200%2C900&ssl=1&resize=350%2C200',
-],
-category: 'Tak Dikenal',
-),
+  TourismPlace(
+    name: 'Gedung Miring Unsika',
+    location: 'Telukjambe, Karawang',
+    description:
+    'Berada di Kampus Terbaik Di Alam Semesta yaitu Universitas Singaperbangsa Karawang. Selalu menjadi bahan perbincangan mahasiswa Fasilkom. Ketika anda melewati kawasan ini, anda akan mencium aura korupsi yang sangat kuat. Hanya Allah yang tau akhir dari nasib gedung ini.',
+    openDays: 'Setiap Hari',
+    openTime: '24 Jam Nonstop',
+    ticketPrice: 'Gratis',
+    imageAsset: 'images/gedung_miring_unsika.jpg',
+    imageUrls: [
+      'https://karawangpos.com/wp-content/uploads/2021/04/fslkm.jpg',
+      'https://i0.wp.com/www.faktajabar.co.id/wp-content/uploads/2019/09/Gedung-Fasilkom_1.jpeg?fit=1036%2C492&ssl=1&resize=350%2C200',
+      'https://i0.wp.com/www.faktajabar.co.id/wp-content/uploads/2022/07/IMG-20220713-WA0040.jpg?fit=1200%2C900&ssl=1&resize=350%2C200',
+    ],
+    category: 'Tak Dikenal',
+  ),
+
+  TourismPlace(
+    name: 'Curug Cigentis',
+    location: 'Tegalwaru, Karawang',
+    description:
+    'Curug Cigentis adalah air terjun dengan ketinggian sekitar 30 meter yang dikelilingi oleh hutan lebat. Tempat ini menawarkan keindahan alam yang asri dan cocok untuk hiking.',
+    openDays: 'Setiap hari',
+    openTime: '08:00 - 17:00 WIB',
+    ticketPrice: 'Rp15.000',
+    imageAsset: 'images/Curug_Cigentis.jpg',
+    imageUrls: [
+      'https://min.co.id/wp-content/uploads/2024/02/500-2.jpg',
+      'https://mediaseruni.id/wp-content/uploads/elementor/thumbs/curug-cigentis-qpld0csb09t8v2lebvtmxz2scpj0owzugudbo9jrcg.jpg',
+      'https://ik.imagekit.io/tvlk/blog/2024/07/A7rMKrdi-shutterstock_2282646145.jpg?tr=c-at_max?tr=c-at_max',
+    ],
+    category: 'Curug',
+  ),
+
+  TourismPlace(
+    name: 'Pantai Tanjung Baru',
+    location: 'Telukjambe, Karawang',
+    description:
+    'Pantai ini menawarkan pasir putih dan ombak yang tenang, cocok untuk menikmati matahari terbenam dan beraktivitas di tepi laut.',
+    openDays: 'Setiap Hari',
+    openTime: '08:00 - 18:00 WIB',
+    ticketPrice: 'Rp10.000',
+    imageAsset: 'images/Pantai_Tanjung_Baru.jpg',
+    imageUrls: [
+      'https://asset-2.tstatic.net/tribunnewswiki/foto/bank/images/Pantai-Tanjung-Baru-di-Karawang.jpg',
+      'https://www.pesisir.net/wp-content/uploads/2021/12/Alamat-Wisata-Pantai-Tanjung-Baru.jpg',
+      'https://i.pinimg.com/736x/d6/a9/65/d6a9653caa7c678e3306aab48cff93e2.jpg',
+    ],
+    category: 'Pantai',
+  ),
 
 ];
